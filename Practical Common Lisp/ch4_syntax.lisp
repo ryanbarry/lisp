262324c670db52d8785c0c91b22cd7204f2d9db9
Ryan Barry <ryan.barry@isocket.com>
2014-10-18 22:42:58 -0700

    add more syntax examples

--- Practical Common Lisp/ch4_syntax.lisp
+++ Practical Common Lisp/ch4_syntax.lisp
@@ -1,9 +1,13 @@
 ;;;; this is just an example of the different parts of syntax gone over in ch4
 
 ;; first, atoms
-19 ; a number
+19 ; an integer
+2/3 ; a ratio
+1.3 ; a single-precision float
+1d3 ; a double-precision float representing 1000.0
 "asdf" ; a string
 '("a" "list" "of" "strings") ; a list of strings
+:id ; a keyword symbol
 
 ;; now sexps
 (if t (format t "asdf")) ; special form if
