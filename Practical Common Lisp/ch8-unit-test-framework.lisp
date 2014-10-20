c829819d760197ccc3379ddd0bfc29c414fdd8f2
Ryan Barry <ryan.barry@isocket.com>
2014-10-19 22:11:23 -0700

    first useful version of test fw, using macro

--- Practical Common Lisp/ch8-unit-test-framework.lisp
+++ Practical Common Lisp/ch8-unit-test-framework.lisp
@@ -1,6 +1,17 @@
+(defmacro my-with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
 
 (defmacro check (&body forms)
-  `(progn
+  `(combine-results
      ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  (my-with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
