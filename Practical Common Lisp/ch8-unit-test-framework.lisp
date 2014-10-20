50b066b60dc5f5c5424cfe2c2f1b10d9e1c87b00
Ryan Barry <ryan.barry@isocket.com>
2014-10-19 22:22:17 -0700

    show current test function in result output

--- Practical Common Lisp/ch8-unit-test-framework.lisp
+++ Practical Common Lisp/ch8-unit-test-framework.lisp
@@ -1,9 +1,13 @@
+;;;; unit test framework
+
+(defvar *test-name* nil) ; used to hold current test
+
 (defmacro my-with-gensyms ((&rest names) &body body)
   `(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
   result)
 
 (defmacro check (&body forms)
@@ -15,3 +19,24 @@
     `(let ((,result t))
        ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
        ,result)))
+
+;;; the following is used to demonstrate usage of the framework from above
+
+(defun test-+ ()
+  (let ((*test-name* 'test-+))
+    (check
+      (= (+ 1 2) 3)
+      (= (+ 1 2 3) 6)
+      (= (+ -1 -3) -4))))
+
+(defun test-* ()
+  (let ((*test-name* 'test-*))
+    (check
+      (= (* 2 2) 4)
+      (= (* 3 5) 15))))
+
+(defun test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+
