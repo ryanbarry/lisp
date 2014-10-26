4922b5d4c9483bf6b0ed7f8857abce52e6605162
Ryan Barry <ryan.barry@isocket.com>
2014-10-25 22:46:29 -0700

    trying out character i/o

--- Practical Common Lisp/ch14_file-io.lisp
+++ Practical Common Lisp/ch14_file-io.lisp
@@ -13,3 +13,10 @@
 	 while line do (format t "~a~%" line))
     (print '(EOF))
     (close file)))
+
+(let ((file (open "./ch14_testfile.txt")))
+  (format t "~%") ; start with a newline, to separate from any previous output
+  (loop for c = (read-char file nil)
+       while c do (format t "~a'" c))
+  (print '(EOF)) ; denote where the file actually ends in output, helpful when
+  (close file))  ; a file ends with a newline or some other non-printing char
