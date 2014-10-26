270897984d577ed953dc27a0239c2921072d7be2
Ryan Barry <ryan.barry@isocket.com>
2014-10-25 22:36:26 -0700

    more ch 14

--- Practical Common Lisp/ch14_file-io.lisp
+++ Practical Common Lisp/ch14_file-io.lisp
@@ -1,3 +1,15 @@
 (let ((file (open "./ch14_testfile.txt")))
   (format t "~a~%" (read-line file))
   (close file))
+
+(let ((file (open "./ch14_testfile.txt" :if-does-not-exist nil)))
+  (when file
+    (format t "~a~%" (read-line file))
+    (close file)))
+
+(let ((file (open "./ch14_testfile.txt" :if-does-not-exist nil)))
+  (when file
+    (loop for line = (read-line file nil)
+	 while line do (format t "~a~%" line))
+    (print '(EOF))
+    (close file)))
